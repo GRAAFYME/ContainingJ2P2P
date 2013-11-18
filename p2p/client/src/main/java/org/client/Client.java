8daed39d96ef881ccc9e046b4d57fccfe65031f9
commandersatisfied <huppel31@hotmail.com>
2013-11-18 14:08:42 +0100

    Changed basics and added water.
    -Added Water
    -Changed cam speed
    -Changed background color

--- p2p/client/src/main/java/org/client/Client.java
+++ p2p/client/src/main/java/org/client/Client.java
@@ -1,16 +1,11 @@
 package org.client;
 
-/**
- * Hello world!
- *
- */
-
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
-import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Node;
 
 /** Sample 1 - how to get started with the most simple JME 3 application.
  * Display a blue 3D cube and view from all sides by
@@ -27,6 +22,8 @@ import com.jme3.math.ColorRGBA;
  * */
 
 public class Client extends SimpleApplication {
+	
+	public Node waterNode;
 
     public static void main(String[] args){
         Client app = new Client();
@@ -35,12 +32,24 @@ public class Client extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1); // create cube shape
+    	testBox();
+        waterNode = new Node("Water");
+        Water water = new Water(assetManager, waterNode);
+        water.initPPcWater();
+        viewPort.addProcessor(water.fpp); 
+        rootNode.attachChild(waterNode);
+    	flyCam.setMoveSpeed(10f);
+    	viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
+        }
+    
+    public void testBox(){
+    	Box b = new Box(1, 1, 1); // create cube shape
         Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
         Material mat = new Material(assetManager,
                 "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
         mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
         geom.setMaterial(mat);                   // set the cube's material
-        rootNode.attachChild(geom);              // make the cube appear in the scene
+        rootNode.attachChild(geom);              // make the cube appear in the scene	
     }
+
 }
---
+++ p2p/client/src/main/java/org/client/Water.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.client;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
+import com.jme3.scene.Node;
+import com.jme3.texture.Texture2D;
+import com.jme3.water.WaterFilter;
+
+/*
+ * Authors
+ * Joshua Bergsma
+ * Remco de Bruin
+ * Yme van der Graaf
+ * Jeffrey Harders
+ * Arjen Pander
+ * Melinda de Roo
+ * */
+public class Water {
+    private AssetManager assetManager;    
+    private WaterFilter water;
+    private Node waterNode;    
+    private Vector3f lightDir;
+    public FilterPostProcessor fpp;
+    
+    public Water(AssetManager am, Node node)
+    {
+        assetManager = am;
+        lightDir =  new Vector3f(-4,-1,5);
+        waterNode = node;
+    }
+     public FilterPostProcessor initPPcWater(){ 
+        fpp = new FilterPostProcessor(assetManager); 
+        water = new WaterFilter(waterNode, lightDir); 
+        water.setCenter(Vector3f.ZERO); 
+        water.setRadius(2600); 
+        water.setWaveScale(0.003f); 
+        water.setMaxAmplitude(2f); 
+        water.setFoamExistence(new Vector3f(1f, -0.5f, 1f)); 
+        water.setFoamTexture((Texture2D) assetManager.loadTexture("Common/MatDefs/Water/Textures/foam2.jpg")); 
+        water.setRefractionStrength(0.2f); 
+        water.setWaterHeight(10f); 
+        fpp.addFilter(water); 
+        return fpp;
+    }
+}
