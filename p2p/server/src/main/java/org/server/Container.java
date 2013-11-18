bf45a8580ab0ac6c8337dbe6749e0522114df2e1
Remco <remcodebruin@gmx.net>
2013-11-18 18:01:32 +0100

    XML parser/container class mostly done

--- p2p/server/src/main/java/org/server/Container.java
+++ p2p/server/src/main/java/org/server/Container.java
@@ -10,12 +10,29 @@ import javax.vecmath.Point3d;
  * To change this template use File | Settings | File Templates.
  */
 public class Container {
-    public Point3d location;
+    public String id;
+    public Point3d location;    //replace Point3D with "better suited" format?
     public Point3d arrivalDate;
     public Point3d leaveDate;
     public String name;
-    //Additional attributes go here, not sure they are relevant at all
+    public String arriveTimeFrom; //Change these from String to Date or Point
+    public String arriveTimeTill; //Change these from String to Date or Point
+    public String leaveTimeFrom;  //Change these from String to Date or Point
+    public String leaveTimeTill;  //Change these from String to Date or Point
+    public String arrivalTransport;//Change these from String to Date or Point;
+    public String leaveTransport;
+    public String arrivalCompany;
+    public String leaveCompany;
+    public double emptyWeight;
+    public double fullWeight;
+    public String contentName;
+    public String contentType;
+    public String contentDanger;
+    public double iso;
 
+    public Container()
+    {
+    }
 
     public Container(Point3d location, Point3d arrival, Point3d leave, String name)
     {
