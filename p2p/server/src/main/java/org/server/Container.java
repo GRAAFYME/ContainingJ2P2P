1c42e1939a7bce28ac6858011537bfe00690447e
Remco <remcodebruin@gmx.net>
2013-11-20 17:49:04 +0100

    xmlParser rewrite, Container being just the xml representation we probably need to create a Container wrapper class

--- p2p/server/src/main/java/org/server/Container.java
+++ p2p/server/src/main/java/org/server/Container.java
@@ -1,46 +1,68 @@
 package org.server;
 
-import javax.vecmath.Point3d;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Remco
- * Date: 18/11/13
- * Time: 11:43
- * To change this template use File | Settings | File Templates.
- */
-public class Container {
-    public String id;
-    public Point3d location;    //replace Point3D with "better suited" format?
-    public Point3d arrivalDate;
-    public Point3d leaveDate;
-    public String name;
-    public String arriveTimeFrom; //Change these from String to Date or Point
-    public String arriveTimeTill; //Change these from String to Date or Point
-    public String leaveTimeFrom;  //Change these from String to Date or Point
-    public String leaveTimeTill;  //Change these from String to Date or Point
-    public String arrivalTransport;//Change these from String to Date or Point;
-    public String leaveTransport;
-    public String arrivalCompany;
-    public String leaveCompany;
-    public double emptyWeight;
-    public double fullWeight;
-    public String contentName;
-    public String contentType;
-    public String contentDanger;
-    public double iso;
+import org.eclipse.persistence.oxm.annotations.XmlPath;
 
-    public Container()
-    {
-    }
-
-    public Container(Point3d location, Point3d arrival, Point3d leave, String name)
-    {
-        this.location = location;
-        this.arrivalDate = arrival;
-        this.leaveDate = leave;
-        this.name = name;
-    }
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 
+@XmlRootElement
+class Container
+{
+    @XmlAttribute
+    String id;
+
+    @XmlPath("aankomst/datum/d/text()")
+    int arrivalDay;
+    @XmlPath("aankomst/datum/m/text()")
+    int arrivalMonth;
+    @XmlPath("aankomst/datum/j/text()")
+    int arrivalYear;
+    @XmlPath("aankomst/tijd/van/text()")
+    String arrivalTill;
+    @XmlPath("aankomst/tijd/tot/text()")
+    String arrivalFrom;
+
+    @XmlPath("vertrek/datum/d/text()")
+    int leaveDay;
+    @XmlPath("vertrek/datum/m/text()")
+    int leaveMonth;
+    @XmlPath("vertrek/datum/j/text()")
+    int leaveYear;
+    @XmlPath("vertrek/tijd/van/text()")
+    String leaveFrom;
+    @XmlPath("vertrek/tijd/tot/text()")
+    String leaveTill;
+    @XmlPath("aankomst/positie/x/text()")
+    int arrivalPosX;
+    @XmlPath("aankomst/positie/y/text()")
+    int arrivalPosY;
+    @XmlPath("aankomst/positie/z/text()")
+    int arrivalPosZ;
+    @XmlPath("eigenaar/naam/text()")
+    String ownerName;
+    @XmlPath("eigenaar/containernr/text()")
+    int nr;
+    @XmlPath("aankomst/soort_vervoer/text()")
+    String arrivalTransportType;
+    @XmlPath("vertrek/soort_vervoer/text()")
+    String leaveTransportType;
+    @XmlPath("aankomst/bedrijf/text()")
+    String arrivalCompany;
+    @XmlPath("vertrek/bedrijf/text()")
+    String leaveCompany;
+    @XmlPath("gewicht/leeg/text()")
+    int emptyWeight;
+    @XmlPath("gewicht/inhoud/text()")
+    int fullWeight;
+    @XmlPath("inhoud/naam/text()")
+    String contentName;
+    @XmlPath("inhoud/soort/text()")
+    String contentType;
+    @XmlPath("inhoud/gevaar/text()")
+    String dangerType;
+    @XmlElement(name="ISO")
+    String iso;
 }
--- p2p/server/src/main/java/org/server/xmlParser.java
+++ p2p/server/src/main/java/org/server/xmlParser.java
@@ -1,150 +1,35 @@
 package org.server;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Remco
- * Date: 18/11/13
- * Time: 11:40
- * To change this template use File | Settings | File Templates.
- */
 
-import java.io.*;
-import java.util.*;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathFactory;
-
-import javax.vecmath.Point3d;
-/**
- * Hello world!
- *
- */
 public class xmlParser
 {
-
-    public List<Container> parse(String path) throws FileNotFoundException
+    public ContainerSet parse(String path) throws FileNotFoundException
     {
         List<Container> containerList = new ArrayList<Container>();
+        ContainerSet containers = null;
+
 
         try {
-            //Configure the xpath stuff
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(path);
-            XPathFactory xPathfactory = XPathFactory.newInstance();
-            XPath xpath = xPathfactory.newXPath();
-            //Grabs all containers, <record></record>
-            XPathExpression recordExpr = xpath.compile("/recordset/record");
+            File file = new File(path);
+            JAXBContext jaxbContext = JAXBContext.newInstance(ContainerSet.class);
+            System.out.println(jaxbContext.getClass());
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+           containers = (ContainerSet) jaxbUnmarshaller.unmarshal(file);
 
-            //:TODO: stop presuming dates are in format d/m/j and locations in x/y/z
-            //Grab arrival date
-            XPathExpression arrivalDExpr = xpath.compile("aankomst/datum/d");
-            XPathExpression arrivalMExpr = xpath.compile("aankomst/datum/m");
-            XPathExpression arrivalJExpr = xpath.compile("aankomst/datum/j");
-            XPathExpression arrivalFromExpr = xpath.compile("aankomst/tijd/van");
-            XPathExpression arrivalTillExpr = xpath.compile("aankomst/tijd/tot");
-            //Grab leave date
-            XPathExpression leaveDExpr = xpath.compile("vertrek/datum/d");
-            XPathExpression leaveMExpr = xpath.compile("vertrek/datum/m");
-            XPathExpression leaveJExpr = xpath.compile("vertrek/datum/j");
-            XPathExpression leaveFromExpr = xpath.compile("vertrek/tijd/van");
-            XPathExpression leaveTillExpr = xpath.compile("vertrek/tijd/tot");
-            //Grab location
-            XPathExpression locationXExpr = xpath.compile("aankomst/positie/*");
-            XPathExpression locationYExpr = xpath.compile("aankomst/positie/*");
-            XPathExpression locationZExpr = xpath.compile("aankomst/positie/*");
-            //Grab name
-            XPathExpression nameExpr = xpath.compile("eigenaar/naam");
-            XPathExpression containerNrExpr = xpath.compile("eigenaar/containernr");
-            //Grab Transport types
-            XPathExpression arrivalTransportXpr = xpath.compile("aankomst/soort_vervoer");
-            XPathExpression leaveTransportXpr = xpath.compile("vertrek/soort_vervoer");
-            //Grab Transport companies
-            XPathExpression arrivalCompaniesXpr = xpath.compile("aankomst/bedrijf");
-            XPathExpression leaveCompaniesXpr = xpath.compile("vertrek/bedrijf");
-            //Grab weights
-            XPathExpression emptyWeightXpr = xpath.compile("gewicht/leeg");
-            XPathExpression fullWeightXpr = xpath.compile("gewicht/inhoud");
-            //Grab content
-            XPathExpression contentXpr = xpath.compile("inhoud/naam");
-            XPathExpression contentTypeXpr = xpath.compile("inhoud/soort");
-            XPathExpression contentDangerlevelXpr = xpath.compile("inhoud/gevaar");
-            //Grab ISO
-            XPathExpression isoExpr = xpath.compile("ISO");
-
-
-            //evaluate the expression, this will grab all <record>... </record> nodes
-            //instead of having a complex maze of switch/loop/if statements we can just
-            //grab everything with a loop and a few more xpath expressions
-            NodeList containerNodes = (NodeList)recordExpr.evaluate(doc, XPathConstants.NODESET);
-
-            for (int i = 0; i < containerNodes.getLength(); i++) {
-                Node n = containerNodes.item(i);
-                Container container = new Container();
-                //Grab ID
-                container.id = n.getAttributes().getNamedItem("id").getTextContent();
-                //Grab Arrive Date
-                double d1 = (Double)arrivalDExpr.evaluate(n, XPathConstants.NUMBER);
-                double m1 = (Double)arrivalMExpr.evaluate(n, XPathConstants.NUMBER);
-                double j1 = (Double)arrivalJExpr.evaluate(n, XPathConstants.NUMBER);
-                container.arrivalDate = new Point3d(d1, m1, j1);
-                //Grab ArriveTime
-                container.arriveTimeFrom = (String)arrivalFromExpr.evaluate(n, XPathConstants.STRING);
-                container.arriveTimeTill = (String)arrivalTillExpr.evaluate(n, XPathConstants.STRING);
-
-                //Grab leave Date (0 1 2) => (d m j)
-                double d2 = (Double)leaveDExpr.evaluate(n, XPathConstants.NUMBER);
-                double m2 = (Double)leaveMExpr.evaluate(n, XPathConstants.NUMBER);
-                double j2 = (Double)leaveJExpr.evaluate(n, XPathConstants.NUMBER);
-                container.leaveDate = new Point3d(d2, m2, j2);
-                //Grab leave time
-                container.leaveTimeFrom = (String)leaveFromExpr.evaluate(n, XPathConstants.STRING);
-                container.leaveTimeTill = (String)leaveTillExpr.evaluate(n, XPathConstants.STRING);
-
-                //TODO: check if the xml file contains float values, if so, change int -> float
-                //Grab location (0 1 2) => (x y z)
-                double x = (Double)locationXExpr.evaluate(n, XPathConstants.NUMBER);
-                double y = (Double)locationYExpr.evaluate(n, XPathConstants.NUMBER);
-                double z = (Double)locationZExpr.evaluate(n, XPathConstants.NUMBER);
-                container.location = new Point3d(x, y, z);
-
-                //Grab name
-                container.name = (String)nameExpr.evaluate(n, XPathConstants.STRING);
-
-                //Grab Transport types
-                container.arrivalTransport = (String)arrivalTransportXpr.evaluate(n, XPathConstants.STRING);
-                container.leaveTransport = (String)leaveTransportXpr.evaluate(n, XPathConstants.STRING);
-                //Grab Transport companies
-                container.arrivalCompany = (String)arrivalCompaniesXpr.evaluate(n, XPathConstants.STRING);
-                container.leaveCompany = (String)leaveCompaniesXpr.evaluate(n, XPathConstants.STRING);
-                //Grab weights
-                container.emptyWeight = (Double)emptyWeightXpr.evaluate(n, XPathConstants.NUMBER);
-                container.fullWeight = (Double)fullWeightXpr.evaluate(n, XPathConstants.NUMBER);
-                //Grab content
-                container.contentName = (String)contentXpr.evaluate(n, XPathConstants.STRING);
-                container.contentType = (String)contentTypeXpr.evaluate(n, XPathConstants.STRING);
-                container.contentDanger = (String)contentDangerlevelXpr.evaluate(n, XPathConstants.STRING);
-                //Grab ISO
-                container.iso = (Double)isoExpr.evaluate(n, XPathConstants.NUMBER);
-
-                containerList.add(container);
-            }
         }
-        catch (Exception e) {
-            System.out.println("Exception in Server.main: " + e.getMessage());
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
         }
 
-        return containerList;
+        return containers;
     }
 }
 
