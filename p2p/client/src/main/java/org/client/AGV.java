69b09b5752d7e3ff7b4d405c33acf1417ef9bc5b
commandersatisfied <huppel31@hotmail.com>
2013-11-26 12:45:56 +0100

    Client: Moved more AGV code
    -Cleaned code
    -Removed temporary code
    -Moved AGV code to AGV
    -Moved Path code to AGV

--- p2p/client/src/main/java/org/client/AGV.java
+++ p2p/client/src/main/java/org/client/AGV.java
@@ -1,55 +1,55 @@
 package org.client;
 
-import com.bulletphysics.collision.broadphase.Dbvt.Node;
 import com.jme3.asset.AssetManager;
 import com.jme3.cinematic.MotionPath;
 import com.jme3.cinematic.MotionPathListener;
 import com.jme3.cinematic.events.MotionEvent;
 import com.jme3.font.BitmapFont;
 import com.jme3.font.BitmapText;
-import com.jme3.input.InputManager;
-import com.jme3.input.KeyInput;
-import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
-import com.jme3.math.Spline.SplineType;
-import com.jme3.renderer.Camera;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+/*
+ * Authors
+ * Joshua Bergsma
+ * Remco de Bruin
+ * Yme van der Graaf
+ * Jeffrey Harders
+ * Arjen Pander
+ * Melinda de Roo 
+ * */
 
-
-
-public final class AGV extends Node{
+public class AGV{
 	
 	private boolean setWireFrame;
     private MotionPath path;
-    private MotionEvent motionControl, motionControl2;
-    private boolean active = true;
-    private boolean playing = false;
-    private boolean playing2 = false;
-	private Spatial sceneModel, AutomaticGuidedVehicle, AGV2;
+    public MotionEvent motionControl, motionControl2;
+	private Spatial AutomaticGuidedVehicle, AGV2;
+	public BitmapText wayPointsText;
+	public Spatial AGV;
 	private AssetManager assetManager;
-	private InputManager inputManager;
-	private Camera cam;
 	private BitmapFont guiFont;
+	public Node nodeAGV;
+	private Node AllAgvNodes;
+	
+	public AGV(Vector3f location, AssetManager am, Node allAgvNodes){
+		AllAgvNodes = allAgvNodes;
+		assetManager = am;
+		tempWaypoint();
+		addAGVcar(location);
+		initMotionControl();
+	}
 	
     public AGV(AssetManager assetManager, boolean setWireFrame){
     	this.assetManager = assetManager;
     	this.setWireFrame = setWireFrame;
-    	//this.path = path;
-    	//this.motionControl = motionControl;
-    	//this.AutomaticGuidedVehicle = AutomaticGuidedVehicle;
-    	//this.AGV2 = AGV2;
-    	//this.inputManager = inputManager;
-    	CreatePath();
-        addAGV(new Vector3f(70f,118.5f,130f));
-        addAGV2(new Vector3f(90f,118.5f,130f));
-       // this.attachChild(AGV, new Vector3f(70f,118.5f,130f));
-        
+    	CreatePath();  
     }
+    
     public void CreatePath(){
     path = new MotionPath();
     path.addWayPoint(new Vector3f(60, 117, 130));
@@ -97,26 +97,56 @@ public final class AGV extends Node{
         }});
 	
     }
-    public void addAGV(Vector3f location){
-    	AutomaticGuidedVehicle = assetManager.loadModel("Models/AGV/AGV.obj" );
-    	AutomaticGuidedVehicle.setLocalTranslation(location);
-    	AutomaticGuidedVehicle.scale(10);
+
+    public void addAGVcar(Vector3f location){
+    	AGV = assetManager.loadModel("Models/AGV/AGV.obj" );
+    	AGV.setLocalTranslation(location);
+    	AGV.scale(10);
     	Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Black);
-        AutomaticGuidedVehicle.setMaterial(mat);
-        AutomaticGuidedVehicle.setName("Name");
-    	mat.getAdditionalRenderState().setWireframe(this.setWireFrame);
+    	AGV.setMaterial(mat);
+    	AGV.setName("Name");
+    	nodeAGV = new Node();
+    	nodeAGV.attachChild(AGV); // make the AGV appear in the scene  	
+    	AllAgvNodes.attachChild(nodeAGV);    }
+
+    public void initMotionControl(){
+    	motionControl = new MotionEvent(AGV, path);
+        motionControl.setDirectionType(MotionEvent.Direction.Path);
+        motionControl.setRotation(new Quaternion().fromAngleNormalAxis(-FastMath.HALF_PI, Vector3f.UNIT_Y));
+        motionControl.setInitialDuration(10f);
+        motionControl.setSpeed(2f);       
+        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
     	
     	
+    	wayPointsText = new BitmapText(guiFont, false);
+        wayPointsText.setSize(guiFont.getCharSet().getRenderedSize());
+    	wayPointsText.setSize(guiFont.getCharSet().getRenderedSize());
+    	
+        path.addListener(new MotionPathListener() {
+
+        public void onWayPointReach(MotionEvent control, int wayPointIndex) {
+                if (path.getNbWayPoints() == wayPointIndex + 1) {
+                    wayPointsText.setText(control.getSpatial().getName() + "Finished!!! ");
+                } else {
+                    wayPointsText.setText(control.getSpatial().getName() + " Reached way point " + wayPointIndex);
+                }
+                //wayPointsText.setLocalTranslation((cam.getWidth() - wayPointsText.getLineWidth()) / 2, cam.getHeight(), 0);
+                wayPointsText.setLocalTranslation(100,100,100);
+        }});
     }
-    public void addAGV2(Vector3f location){
-    	AGV2 = assetManager.loadModel("Models/AGV/AGV.obj" );
-    	AGV2.setLocalTranslation(location);
-    	AGV2.scale(10);
-    	Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Red);
-    	AGV2.setMaterial(mat);
-    	AGV2.setName("Name2");
-      //  rootNode.attachChild(AGV2);              // make the AGV appear in the scene	
+
+    public void tempWaypoint(){
+    	path = new MotionPath();
+        path.addWayPoint(new Vector3f(60, 117, 130));
+        path.addWayPoint(new Vector3f(60, 117, 240));
+        path.addWayPoint(new Vector3f(124, 117, 240));
+        path.addWayPoint(new Vector3f(124, 117, -141));
+        path.addWayPoint(new Vector3f(-97, 117, -141));
+        path.addWayPoint(new Vector3f(-97, 117, 11));
+        path.addWayPoint(new Vector3f(-141, 117, 11));
+        path.setCurveTension(0.3f);
+        path.enableDebugShape(assetManager, AllAgvNodes);
+        
     }
 }
