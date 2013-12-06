55fb795aaf1542dce9c26f78b60bf0c1d9759d1b
Melinda de Roo <mellirulez@gmail.com>
2013-12-06 13:27:15 +0100

    Crane class update

--- p2p/client/src/main/java/org/client/Crane.java
+++ p2p/client/src/main/java/org/client/Crane.java
@@ -1,7 +1,42 @@
 package org.client;
 
-public class Crane {
-	
+import com.jme3.cinematic.MotionPath;
+import com.jme3.cinematic.MotionPathListener;
+import com.jme3.cinematic.events.MotionEvent;
+import com.jme3.math.Vector3f;
+
+public class Crane implements MotionPathListener
+{
 	//TODO: Add methods here for crane movement/motionpath
+	//Vectors come in the specific classes
+	
+	MotionPath path;
+	Vector3f posCrane, posHook, posSlider, destination;
+	
+	public Crane()
+	{
+		moveCrane(new Vector3f(10,0,0), 0.01f);
+	}
+	
+	public void moveCrane(Vector3f des, float sp)
+	{
+		path.addWayPoint(des);
+		System.out.println(des);
+	}
+	
+	public void moveHook(Vector3f des, float sp)
+	{
+		
+	}
+	
+	public void moveSlider(Vector3f des, float sp)
+	{
+		
+	}
+
+	public void onWayPointReach(MotionEvent arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
