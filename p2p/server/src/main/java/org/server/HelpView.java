d5341969e63270b5ca808e37a502b47cdc4e46ee
Joshua <joshua_judo12@hotmail.com>
2013-12-31 12:07:51 +0100

    Added some Text To Help

--- p2p/server/src/main/java/org/server/HelpView.java
+++ p2p/server/src/main/java/org/server/HelpView.java
@@ -51,20 +51,21 @@ public class HelpView
 		//tabbedPane.setBounds(10, 10, 550, 300);
 		mainpanel.add(tabbedPane);
 		//ImageIcon icon = new ImageIcon("icon.png");
-		tabbedPane.addTab("Help", makePanel("Help"));
+		tabbedPane.addTab("Help", makePanel("Hello and welcome to the Help Menu!" + "\n" + "\n" + "If you need some help then this is the perfect place for you!"));
 		//System.out.println(getClass().getResource("data/icon.png"));
-		tabbedPane.addTab("How to get started!", makePanel("How to get started!"));
+		tabbedPane.addTab("How to get started!", makePanel("How to get started!" + "\n" + "\n" + "1. Load a XML-File from the File -> Load File option" + "\n" + "2. Dont start the server if the Client is not open! (else it does nothing)" + "\n" + "3. If you want to update the website then Login to ftp!" + "\n" + "4. Start the Server!"));
 			
 	}
 	
 
-	 private static JPanel makePanel(String text) 
+	 private JPanel makePanel(String text) 
 	 {
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
 		//JLabel Textlabel = new JLabel(text);
 		//Textlabel.setBounds(5,0,400,400);
 		JTextArea textArea = new JTextArea(text);
+		
 		textArea.setBounds(0,0,600,400);
 		panel.add(textArea);
 		//panel.setLayout(new GridLayout(1, 1));
