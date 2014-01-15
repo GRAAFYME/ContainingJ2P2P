2e73a412a7450b14b66effc89da7fce21569c3f4
GRAAFYME <graaf.yme91@hotmail.com>
2014-01-15 13:35:01 +0100

    Revert 'Client: Renamed variables'

--- p2p/client/src/main/java/org/client/Crane.java
+++ p2p/client/src/main/java/org/client/Crane.java
@@ -25,13 +25,8 @@ public abstract class Crane extends Node {
 	protected Node hookNode = new Node();
 	protected Node sliderNode = new Node();
 	private String id;
-<<<<<<< HEAD
-	private boolean inProgress;
-	
-=======
 	private boolean busy;
 
->>>>>>> b33be5d4798a315c1042a3eee8087ef10bded7cc
 	private Vector3f position;
 
 	public Crane(String id, Vector3f position, Spatial crane, Spatial slider,
@@ -71,16 +66,9 @@ public abstract class Crane extends Node {
 	public String getId() {
 		return this.id;
 	}
-<<<<<<< HEAD
-	
-	public boolean isBusy()
-	{
-		return inProgress;
-=======
 
 	public boolean isBusy() {
 		return busy;
->>>>>>> b33be5d4798a315c1042a3eee8087ef10bded7cc
 	}
 
 	public Vector3f getPosition() {
@@ -118,73 +106,6 @@ public abstract class Crane extends Node {
 
 		return distance;
 	}
-<<<<<<< HEAD
-	
-	public void animation(int road, final Vector3f[] des, final float sp)
-	{
-		switch(road)
-		{
-			case 1:
-			{
-				//AGV to Storage
-				this.inProgress = true;
-				moveCrane(des[3], sp);
-				pathCrane.addListener(new MotionPathListener()
-				{
-					public void onWayPointReach(MotionEvent me, int index) 
-					{
-						if(pathCrane.getNbWayPoints() == index + 1)
-						{
-							craneControl.stop();
-							moveHook(des[2], sp);
-							pathHook.addListener(new MotionPathListener()
-							{
-								public void onWayPointReach(MotionEvent me2,int index2) 
-								{
-									if(pathHook.getNbWayPoints() == index2 + 1)
-									{
-										hookControl.stop();
-										moveCrane(des[0], sp);
-										pathCrane.addListener(new MotionPathListener()
-										{
-											public void onWayPointReach(MotionEvent me3, int index3)
-											{
-												if(pathCrane.getNbWayPoints() == index3 + 1)
-												{
-													craneControl.stop();
-													hookControl.stop();
-													moveSlider(des[1], sp);
-													pathSlider.addListener(new MotionPathListener()
-													{
-														public void onWayPointReach(MotionEvent me4, int index4)
-														{
-															if(pathSlider.getNbWayPoints() == index4 + 1)
-															{
-																sliderControl.stop();
-																moveHook(des[5], sp);
-																pathHook.addListener(new MotionPathListener()
-																{
-																	public void onWayPointReach(MotionEvent me5, int index5) 
-																	{
-																		if(pathHook.getNbWayPoints() == index5 + 1)
-																		{
-																			hookControl.stop();
-																			craneControl.stop();
-																			moveSlider(des[4], sp);
-																			pathSlider.addListener(new MotionPathListener()
-																			{
-
-																				public void onWayPointReach(MotionEvent me6, int index6)
-																				{
-																					if(pathSlider.getNbWayPoints() == index6 + 1)
-																					{
-																						sliderControl.stop();
-																						hookControl.stop();
-																						//System.out.println("CraneControl: " + craneControl.getPlayState());
-																						//System.out.println("SliderControl: " + sliderControl.getPlayState());
-																						//System.out.println("HookControl: " + hookControl.getPlayState());
-																						//System.exit(1);
-=======
 
 	public void animation(int road, final Vector3f[] des, final float sp) {
 		switch (road) {
@@ -264,7 +185,6 @@ public abstract class Crane extends Node {
 																									}
 
 																								});
->>>>>>> b33be5d4798a315c1042a3eee8087ef10bded7cc
 																					}
 
 																				}
@@ -282,72 +202,6 @@ public abstract class Crane extends Node {
 
 						});
 					}
-<<<<<<< HEAD
-					
-				});
-				this.inProgress = false;
-				break;
-			}
-			case 2:
-			{
-				//Storage to AVG
-				this.inProgress = true;
-				moveCrane(des[0], sp);
-				pathCrane.addListener(new MotionPathListener()
-				{
-					public void onWayPointReach(MotionEvent me, int index) 
-					{
-						if(pathCrane.getNbWayPoints() == index + 1)
-						{
-							craneControl.stop();
-							moveSlider(des[1], sp);
-							pathSlider.addListener(new MotionPathListener()
-							{
-								public void onWayPointReach(MotionEvent me2, int index2)
-								{
-									if(pathSlider.getNbWayPoints() == index2 + 1)
-									{
-										sliderControl.stop();
-										moveHook(des[5], sp);
-										pathHook.addListener(new MotionPathListener()
-										{
-											public void onWayPointReach(MotionEvent me3, int index3)
-											{
-												if(pathHook.getNbWayPoints() == index3 + 1)
-												{
-													hookControl.stop();
-													moveSlider(des[4], sp);
-													pathSlider.addListener(new MotionPathListener()
-													{
-														public void onWayPointReach(MotionEvent me4, int index4)
-														{
-															if(pathSlider.getNbWayPoints() == index4 + 1)
-															{
-																sliderControl.stop();
-																hookControl.stop();
-																moveCrane(des[3], sp);
-																pathCrane.addListener(new MotionPathListener()
-																{
-																	public void onWayPointReach(MotionEvent me5, int index5)
-																	{
-																		if(pathCrane.getNbWayPoints() == index5 + 1)
-																		{
-																			craneControl.stop();
-																			sliderControl.stop();
-																			moveHook(des[2], sp);
-																			pathHook.addListener(new MotionPathListener()
-																			{
-																				public void onWayPointReach(MotionEvent me6, int index6)
-																				{
-																					if(pathHook.getNbWayPoints() == index6 + 1)
-																					{
-																						hookControl.stop();
-																						sliderControl.stop();
-																						//System.out.println("CraneControl: " + craneControl.getPlayState());
-																						//System.out.println("SliderControl: " + sliderControl.getPlayState());
-																						//System.out.println("HookControl: " + hookControl.getPlayState());
-																						//System.exit(1);
-=======
 				}
 
 			});
@@ -426,7 +280,6 @@ public abstract class Crane extends Node {
 																								}
 																							}
 																						});
->>>>>>> b33be5d4798a315c1042a3eee8087ef10bded7cc
 																					}
 																				}
 																			});
@@ -440,54 +293,6 @@ public abstract class Crane extends Node {
 							}
 						});
 					}
-<<<<<<< HEAD
-				});
-				this.inProgress = false;
-				break;
-			}
-			case 3:
-			{
-				//Truck to AGV
-				this.inProgress = true;
-				moveCrane(des[2], sp);
-				pathCrane.addListener(new MotionPathListener()
-				{
-					public void onWayPointReach(MotionEvent me, int index) 
-					{
-						if(pathCrane.getNbWayPoints() == index + 1)
-						{
-							craneControl.stop();
-							moveHook(des[1], sp);
-							pathHook.addListener(new MotionPathListener()
-							{
-								public void onWayPointReach(MotionEvent me2, int index2)
-								{
-									if(pathHook.getNbWayPoints() == index2 + 1)
-									{
-										hookControl.stop();
-										moveCrane(des[0], sp);
-										pathCrane.addListener(new MotionPathListener()
-										{
-											public void onWayPointReach(MotionEvent me3, int index3)
-											{
-												if(pathCrane.getNbWayPoints() == index3 + 1)
-												{
-													craneControl.stop();
-													hookControl.stop();
-													moveHook(des[1], sp);
-													pathHook.addListener(new MotionPathListener()
-													{
-														public void onWayPointReach(MotionEvent me4,int index4)
-														{
-															if(pathHook.getNbWayPoints() == index4 + 1)
-															{
-																hookControl.stop();
-																craneControl.stop();
-																//System.out.println("CraneControl: " + craneControl.getPlayState());
-																//System.out.println("SliderControl: " + sliderControl.getPlayState());
-																//System.out.println("HookControl: " + hookControl.getPlayState());
-																//System.exit(1);
-=======
 				}
 			});
 			this.busy = false;
@@ -539,7 +344,6 @@ public abstract class Crane extends Node {
 																	// hookControl.getPlayState());
 																	// System.exit(1);
 																}
->>>>>>> b33be5d4798a315c1042a3eee8087ef10bded7cc
 															}
 														});
 													}
@@ -549,54 +353,6 @@ public abstract class Crane extends Node {
 							}
 						});
 					}
-<<<<<<< HEAD
-				});
-				this.inProgress = false;
-				break;
-			}
-			case 4:
-			{
-				//AGV to Truck
-				this.inProgress = true;
-				moveHook(des[1], sp);
-				pathHook.addListener(new MotionPathListener()
-				{
-					public void onWayPointReach(MotionEvent me, int index) 
-					{
-						if(pathHook.getNbWayPoints() == index + 1)
-						{
-							hookControl.stop();
-							moveCrane(des[2], sp);
-							pathCrane.addListener(new MotionPathListener()
-							{
-								public void onWayPointReach(MotionEvent me2, int index2)
-								{
-									if(pathCrane.getNbWayPoints() == index2 + 1)
-									{
-										craneControl.stop();
-										moveHook(des[1], sp);
-										pathHook.addListener(new MotionPathListener()
-										{
-											public void onWayPointReach(MotionEvent me3, int index3)
-											{
-												if(pathHook.getNbWayPoints() == index3 + 1)
-												{
-													hookControl.stop();
-													craneControl.stop();
-													moveCrane(des[0], sp);
-													pathCrane.addListener(new MotionPathListener()
-													{
-														public void onWayPointReach(MotionEvent me4,int index4)
-														{
-															if(pathCrane.getNbWayPoints() == index4 + 1)
-															{
-																craneControl.stop();
-																hookControl.stop();
-																//System.out.println("CraneControl: " + craneControl.getPlayState());
-																//System.out.println("SliderControl: " + sliderControl.getPlayState());
-																//System.out.println("HookControl: " + hookControl.getPlayState());
-																//System.exit(1);
-=======
 				}
 			});
 			this.busy = false;
@@ -646,7 +402,6 @@ public abstract class Crane extends Node {
 																	// hookControl.getPlayState());
 																	// System.exit(1);
 																}
->>>>>>> b33be5d4798a315c1042a3eee8087ef10bded7cc
 															}
 														});
 											}
@@ -656,18 +411,11 @@ public abstract class Crane extends Node {
 							}
 						});
 					}
-<<<<<<< HEAD
-				});
-				this.inProgress = false;
-				break;
-			}
-=======
 				}
 			});
 			this.busy = false;
 			break;
 		}
->>>>>>> b33be5d4798a315c1042a3eee8087ef10bded7cc
 		}
 	}
 
--- p2p/client/src/main/java/org/client/Scene.java
+++ p2p/client/src/main/java/org/client/Scene.java
@@ -14,50 +14,13 @@ import com.jme3.scene.Spatial;
 
 public class Scene {
 	public Node sceneNode;
-<<<<<<< HEAD
-	private BulletAppState bulletAppState;  //Physics machine
-	RigidBodyControl rigidBodyControl;
-	CollisionShape sceneShape;   //gives collisions to the scene
-=======
 	private BulletAppState bulletAppState; // Physics machine
 	RigidBodyControl rbc;
 	CollisionShape sceneShape; // gives collisions to the scene
->>>>>>> b33be5d4798a315c1042a3eee8087ef10bded7cc
 	Spatial sceneModel;
 	AssetManager assetManager;
 	public static PointLight sunLight;
 	public static AmbientLight ambient;
-<<<<<<< HEAD
-	
-	//constructor
-	public Scene(BulletAppState Bull, AssetManager am){
-	    assetManager = am;
-	    bulletAppState = Bull;
-	    sceneNode = new Node();
-	    initScene();
-	}   	
-	//creates and loads the scene    
-    public void initScene(){
-    	//sceneModel = assetManager.loadModel("/Scenes/PlatformScene.j3o");
-    	sceneModel = assetManager.loadModel("/Models/platformmodel/platformmodel.j3o");
-        sceneShape = CollisionShapeFactory.createMeshShape(sceneModel);
-        rigidBodyControl = new RigidBodyControl(sceneShape, 0);
-        sceneNode.attachChild(sceneModel);   //adds the sceneModel to the world
-        sceneModel.addControl(rigidBodyControl); 
-        bulletAppState.getPhysicsSpace().add(rigidBodyControl); 
-        initLight();  
-    }
-    //creates and loads the light
-    public void initLight(){
-    	sunLight = new PointLight();
-        sunLight.setPosition(new Vector3f(650, 2000, 40));
-        sunLight.setColor(ColorRGBA.White.mult(1f));
-        sunLight.setRadius(3000f);
-        
-        ambient = new AmbientLight();  //creates a light in the scene, which lights everything from all angles
-        ambient.setColor(ColorRGBA.White.mult(5f));   
-    }
-=======
 
 	// constructor
 	public Scene(BulletAppState Bull, AssetManager am) {
@@ -91,5 +54,4 @@ public class Scene {
 										// lights everything from all angles
 		ambient.setColor(ColorRGBA.White.mult(5f));
 	}
->>>>>>> b33be5d4798a315c1042a3eee8087ef10bded7cc
 }
